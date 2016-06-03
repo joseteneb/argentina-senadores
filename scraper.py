94f09d236bbfc1d242d9153ca59755fcaeb45568
Jose Teneb <joseteneb@gmail.com>
2016-06-03 17:49:50 +0100

    first version

--- scraper.py
+++ scraper.py
@@ -1,24 +1,41 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import scraperwiki
+import lxml.html
+import re
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+def name_reverse(name):
+	reverse = name.split(',  ')
+	reverse.reverse()
+	return ' '.join(reverse)
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def scraper_list(html):
+	root = lxml.html.fromstring(html)
+	for tr in root.cssselect('table.table tbody tr'):
+		tds = tr.cssselect('td')
+		id =  tds[0].cssselect("a")[0].get('href').split('/').pop()
+		data = {
+			'id': id,
+			'image': 'http://www.senado.gov.ar'+tds[0].cssselect('img')[0].get('src'),
+			'name':  name_reverse(re.sub(r'[\n\t]+', ' ', tds[1].text_content().strip())),
+			'sort_name': re.sub(r'[\n\t]+', ' ', tds[1].text_content().strip()),
+			'district':  re.sub(r'[\n\t]+', ' ', tds[2].text_content().strip()),
+			'party':  re.sub(r'[\n\t]+', ' ', tds[3].text_content().strip()),
+			'mandate_start':  tds[4].text_content().split("\n")[0].strip(),
+			'mandate_end':  tds[4].text_content().split("\n")[1].strip(),
+			'email':  tds[5].cssselect('a')[0].text_content().strip(),		
+			'source': 'http://www.senado.gov.ar'+tds[0].cssselect("a")[0].get('href')
+		}	
+		data['phone'] = tds[5].text_content().split('\n')[2].strip() + ' ' + tds[5].text_content().split('\n')[4].strip()
+		if len(tds[5].cssselect('a')) > 1: 
+			link = tds[5].cssselect('a')[1].get('href').strip()
+			if 'facebook' in link:
+				data['facebook'] = tds[5].cssselect('a')[1].get('href').strip()
+			else: 
+				data['twitter'] = tds[5].cssselect('a')[1].get('href').strip()
+		if len(tds[5].cssselect('a')) > 2: 
+			data['twitter'] = tds[5].cssselect('a')[2].get('href').strip()
+		
+		#print data
+		scraperwiki.sqlite.save(unique_keys=['id'], data=data)
+
+
+scraper_list(scraperwiki.scrape('http://www.senado.gov.ar/senadores/listados/listaSenadoRes'))
